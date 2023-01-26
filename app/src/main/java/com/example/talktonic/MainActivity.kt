a61bf7f7c3c1c5fc8696a722a377a477d09e70d3
Rakesh Chandrapal <rakesh202078@gmail.com>
2023-01-26 15:51:38 +0530

    User Profile Photo download and get showed

--- app/src/main/java/com/example/talktonic/MainActivity.kt
+++ app/src/main/java/com/example/talktonic/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.talktonic
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.talktonic.authenticate.LoginActivity
 import com.example.talktonic.databinding.ActivityMainBinding
 import com.google.firebase.auth.ktx.auth
@@ -13,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit  var binding : ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +27,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity,LoginActivity::class.java))
             finish()
         }
-        binding.btnLogout.setOnClickListener{
-            Firebase.auth.signOut()
-            startActivity(Intent(this@MainActivity,LoginActivity::class.java))
-            finish()
-        }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+//        binding.btnLogout.setOnClickListener{
+//            Firebase.auth.signOut()
+//            startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+//            finish()
+//        }
     }
 
 
--- app/src/main/java/com/example/talktonic/userDetails/ShowUserDetailFragment.kt
+++ app/src/main/java/com/example/talktonic/userDetails/ShowUserDetailFragment.kt
@@ -1,23 +1,33 @@
 package com.example.talktonic.userDetails
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.circleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.example.talktonic.R
+import com.example.talktonic.authenticate.LoginActivity
+import com.example.talktonic.databinding.FragmentShowUserDetailBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.InputStream
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShowUserDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShowUserDetailFragment : Fragment() {
+    private val TAG = "ShowUserDetailFragment"
+    private lateinit var  binding : FragmentShowUserDetailBinding
+
+    private lateinit var firebaseRef : FirebaseStorage
+    private lateinit var storage: StorageReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +39,58 @@ class ShowUserDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentShowUserDetailBinding.inflate(layoutInflater,container,false)
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_user_detail, container, false)
+        return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseRef = FirebaseStorage.getInstance()
+        storage = firebaseRef.reference
+
+
+        binding.signOutButton.setOnClickListener{
+            Firebase.auth.signOut()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+
+        }
+
+        loadProfilePhoto()
+    }
+
+    private fun  loadProfilePhoto(){
+        val currentUser = Firebase.auth.currentUser
+        if(currentUser != null) {
+            Log.d(TAG,"Loading From FireBase ${currentUser.uid.toString() } hii ")
+
+            val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(
+                "gs://talk-tonic-88858.appspot.com/User Profiles/${currentUser.uid}.png")
+
+
+            val option = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+
+            storageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener { bytes ->
+                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                Glide.with(requireContext())
+                    .load(bitmap)
+                    .apply(option)
+                    .error(R.drawable.ic_user_default)
+                    .into(binding.userPhotoProfile)
+
+                    Log.d(TAG,"Load profile image SuccessFully ")
+            }
+                .addOnFailureListener{
+                    Log.d(TAG,"Can not load profile image ")
+                }
+        }
+    }
 }
